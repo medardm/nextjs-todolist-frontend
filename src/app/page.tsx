fc0f0279ab6e809e659387c16da41bf9dab16439
Medard Mandane <medardm13@gmail.com>
2024-05-08 18:42:33 +0800

    Refactor TodoList into separate components and add type definitions
    The TodoList and TodoItem have been refactored into separate components in order to make the code more modular and easier to maintain. This commit also includes the addition of type definitions for the Todo item and its associated properties and handlers.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 import React, {useEffect, useReducer, useState} from "react";
-import {IoClose, IoAdd, IoTrash} from "react-icons/io5";
+import {IoAdd, IoClose, IoTrash} from "react-icons/io5";
 import {todoReducer} from "@/reducers";
+import {Todo, TodoItemProps, TodoListProps} from "@/types";
 
 export default function Home() {
   const [newTodo, setNewTodo] = useState('');
   const [
     todoState, dispatchTodo
-  ] = useReducer(todoReducer, { todos: [], loading: false, error: null } );
+  ] = useReducer(todoReducer, {todos: [], loading: false, error: null});
 
   const {todos, error, loading} = todoState
 
@@ -19,7 +20,7 @@ export default function Home() {
       {id: Date.now() + 4, task: 'Get Hired', done: false},
     ]
 
-    dispatchTodo({ type: 'FETCH_TODO_SUCCESS', payload: testTodo })
+    dispatchTodo({type: 'FETCH_TODO_SUCCESS', payload: testTodo})
 
     // TODO: get data from API
     // dispatchTodo({ type: 'FETCH_TODO_REQUEST' });
@@ -31,95 +32,123 @@ export default function Home() {
 
   const handleAddTodo = () => {
     if (newTodo) {
-      dispatchTodo({ type: 'ADD_TODO', task: newTodo });
+      dispatchTodo({type: 'ADD_TODO', task: newTodo});
     }
     setNewTodo('');
   };
 
   const handleRemoveTodo = (id: number) => {
-    dispatchTodo({ type: 'DELETE_TODO', id: id });
+    dispatchTodo({type: 'DELETE_TODO', id: id});
     setNewTodo('');
   };
 
   const handleToggleDone = (id: number) => {
-    dispatchTodo({ type: 'TOGGLE_DONE', id });
+    dispatchTodo({type: 'TOGGLE_DONE', id});
   };
 
   const handleClearDone = () => {
-    dispatchTodo({ type: 'CLEAR_FINISHED' });
+    dispatchTodo({type: 'CLEAR_FINISHED'});
   };
 
   const remainingTodo = todos.filter(todo => !todo.done);
   const completedTodo = todos.filter(todo => todo.done);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
-        <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
-          <div className="mb-4">
-            <h1 className="text-grey-darkest">Todo List <span>({completedTodo.length}/{todos.length})</span></h1>
-            <div className="flex mt-4">
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
-                     placeholder="Add Todo"
-                     onChange={(e) => setNewTodo(e.target.value)}
-                     value={newTodo}
-              />
-              <button
-                onClick={() => handleAddTodo()}
-                className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-black hover:bg-teal flex items-center">
-                <IoAdd size="20px" color="gray" />
-                <span>Add </span>
-              </button>
-            </div>
-          </div>
-
-          <fieldset>
-            {todos.map(todo =>
-              <TodoItem key={todo.id}
-                        id={todo.id}
-                        task={todo.task}
-                        done={todo.done}
-                        handleRemoveTodo={handleRemoveTodo}
-                        toggleTodoDone={handleToggleDone}
-              />
-            )}
-          </fieldset>
-          <div className='mt-3'>
-            <button
-              onClick={() => handleClearDone()}
-              className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-black hover:bg-teal flex gap-1 items-center">
-              <IoTrash size="20px" color="gray" />
-              <span>Clear finished</span>
-            </button>
-          </div>
-        </div>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="min-h-100 w-full flex items-center justify-center bg-teal-lightest">
+        <TodoList
+          todos={todos}
+          newTodo={newTodo}
+          setNewTodo={setNewTodo}
+          handleAddTodo={handleAddTodo}
+          completedTodo={completedTodo}
+          handleRemoveTodo={handleRemoveTodo}
+          handleToggleDone={handleToggleDone}
+          handleClearDone={handleClearDone}
+        />
       </div>
     </main>
   );
 }
 
-type TodoItem = { id: number; task: string; done: boolean, toggleTodoDone: any, handleRemoveTodo: any }
+const TodoList = (
+  {
+    todos,
+    newTodo,
+    setNewTodo,
+    handleAddTodo,
+    completedTodo,
+    handleRemoveTodo,
+    handleToggleDone,
+    handleClearDone
+  }: TodoListProps) => {
+  return (
+    <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+      <div className="mb-4">
+        <h1 className="text-grey-darkest">Todo List <span>({completedTodo.length}/{todos.length})</span></h1>
+        <div className="flex mt-4">
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                 placeholder="Add Todo"
+                 onChange={(e) => setNewTodo(e.target.value)}
+                 value={newTodo}
+          />
+          <button
+            onClick={() => handleAddTodo()}
+            className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-black hover:bg-teal flex items-center">
+            <IoAdd size="20px" color="gray"/>
+            <span>Add </span>
+          </button>
+        </div>
+      </div>
 
-const TodoItem = ({id, task, done, toggleTodoDone, handleRemoveTodo}: TodoItem) => {
-  const doneStyle = done? 'text-gray-400 line-through' : 'text-gray-900'
+      <fieldset>
+        {todos.map((t: Todo) =>
+          <TodoItem key={t.id}
+                    todo={
+                      {
+                        id: t.id,
+                        task: t.task,
+                        done: t.done
+                      }
+                    }
+                    handleRemoveTodo={handleRemoveTodo}
+                    toggleTodoDone={handleToggleDone}
+          />
+        )}
+      </fieldset>
+      <div className='mt-3'>
+        <button
+          onClick={() => handleClearDone()}
+          className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-black hover:bg-teal flex gap-1 items-center">
+          <IoTrash size="20px" color="gray"/>
+          <span>Clear finished</span>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+const TodoItem = ({todo, toggleTodoDone, handleRemoveTodo}: TodoItemProps) => {
+  const doneStyle = todo.done ? 'text-gray-400 line-through' : 'text-gray-900'
 
   return (
     <div className={"flex justify-between items-center border-b-2"}>
       <div className={`flex items-center py-2 flex-grow  ${doneStyle}`}
-           onClick={() => toggleTodoDone(id)}>
+           onClick={() => toggleTodoDone(todo.id)}>
         <input
-          checked={done}
-          id={"todo-" + id}
+          checked={todo.done}
+          id={"todo-" + todo.id}
           type="checkbox"
-          onChange={() => {}}
+          onChange={() => {
+          }}
           className="w-4 h-4 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
           readOnly
         />
-        <label htmlFor={"todo-" + id} className="ms-2 text-md font-medium pointer-events-none">{task}</label>
+        <label htmlFor={"todo-" + todo.id} className="ms-2 text-md font-medium pointer-events-none">{todo.task}</label>
       </div>
       <IoClose size="20px" color="gray" className='cursor-pointer' onClick={e => {
-        handleRemoveTodo(id)
-      }} />
+        handleRemoveTodo(todo.id)
+      }}/>
     </div>
   );
 }
---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export type Todo = {
+  id: number;
+  task: string;
+  done: boolean;
+};
+
+// This type represents an event handler function
+type Handler = (id: number) => void;
+
+// Let's define functions for toggling and removing todos
+type ToggleTodo = (id: number) => void;
+type RemoveTodo = (id: number) => void;
+
+// This represents the props for the TodoList function
+export type TodoListProps = {
+  todos: Todo[];  // change this from TodoItemProps[] to Todo[]
+  newTodo: string;
+  setNewTodo: (value: string) => void;
+  handleAddTodo: () => void;
+  completedTodo: Todo[];
+  handleRemoveTodo: Handler;
+  handleToggleDone: Handler;
+  handleClearDone: () => void;
+};
+
+export type TodoItemProps = {
+  todo: Todo; // change this from separate properties to a single todo of type Todo
+  toggleTodoDone: ToggleTodo; // define the correct type instead of any
+  handleRemoveTodo: RemoveTodo; // define the correct type instead of any
+}
