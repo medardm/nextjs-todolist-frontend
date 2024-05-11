f7f8576557f1768bb5784cdc7998941ccc72a7ff
Medard Mandane <medardm13@gmail.com>
2024-05-11 10:40:40 +0800

    Updated user interface in NavBar and LoginModal
    - Modified NavBar to include current user's username next to the profile picture.
    - Updated title in LoginModal from "Add Product" to "Login" to accurately reflect its functionality.

--- src/components/auth/LoginModal.tsx
+++ src/components/auth/LoginModal.tsx
@@ -37,7 +37,7 @@ export const LoginModal = () => {
             <div
               className="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
               <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                Add Product
+                Login
               </h3>
               <button type="button"
                       className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -65,17 +65,18 @@ export default function Navbar() {
                 </>}
 
                 {user && <button onClick={toggleNav} type="button"
-                         className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                         id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                  <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">Open user menu</span>
-                  <Image
-                    src={"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"}
-                    alt=''
-                    className='h-8 w-8 rounded-full'
-                    width={100}
-                    height={100}
-                  />
+                                 className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 items-center gap-2 p-1"
+                                 id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                    <span className="absolute -inset-1.5"></span>
+                    <span className="sr-only">Open user menu</span>
+                    <Image
+                        src={"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"}
+                        alt=''
+                        className='h-8 w-8 rounded-full'
+                        width={100}
+                        height={100}
+                    />
+                    <span>{ user.user.username }</span>
                 </button>}
               </div>
 
@@ -88,17 +89,17 @@ export default function Navbar() {
                 From: "transform opacity-100 scale-100"
                 To: "transform opacity-0 scale-95"*/}
               {user && <div
-                className={`${navOpen ? '' : 'hidden'} absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
-                role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
+                  className={`${navOpen ? '' : 'hidden'} absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+                  role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
                 {/*Active: "bg-gray-100", Not Active: ""*/}
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1}
-                   id="user-menu-item-0">Profile (TODO)</a>
-                <a href="#" onClick={() => {
-                  toggleNav()
-                  logout()
-                }} className="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                   tabIndex={-1}
-                   id="user-menu-item-2">Sign out</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1}
+                     id="user-menu-item-0">Profile (TODO)</a>
+                  <a href="#" onClick={() => {
+                    toggleNav()
+                    logout()
+                  }} className="block px-4 py-2 text-sm text-gray-700" role="menuitem"
+                     tabIndex={-1}
+                     id="user-menu-item-2">Sign out</a>
               </div>}
             </div>
           </div>
