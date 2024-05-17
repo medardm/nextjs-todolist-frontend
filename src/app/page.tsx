74fc471c4d03360631cf877cf474c265632c9179
Medard Mandane <medardm13@gmail.com>
2024-05-17 21:44:05 +0800

    Add user-specific greeting on homepage
    - Implemented usage of the useAuthStore hook to extract the user's details.
    - Changed the home page layout to include a personalized greeting message for the user stating the current number of todo lists.
    - Incorporated a fallback message for guest users.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import {TodoList} from "@/components/todolist/TodoList";
 import {useTodoList} from "@/hooks/useTodoList";
 import {TodoListNew} from "@/components/form/TodoListNew";
+import useAuthStore from "@/stores/useAuthStore";
 
 export default function Home() {
-
+  const {user} = useAuthStore()
   const {
     todoLists,
     newTodoListInput,
@@ -20,7 +21,12 @@ export default function Home() {
     handleAddTodoList
   }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center py-3">
+      <header className="">
+        <h1 className="text-2xl">
+          Hi {user?.user.username ?? 'Guest'}, You have {todoLists.length} todolist{todoLists.length>1? 's': ''}, please create more
+        </h1>
+      </header>
       <div className="grid grid-cols-3 min-h-100 w-full bg-teal-lightest container mx-auto">
         {
           todoLists.map(
