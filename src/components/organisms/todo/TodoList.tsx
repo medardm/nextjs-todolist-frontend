ec10a6ebddb0ed9962da2039d72571706acff3fe
Medard Mandane <medardm13@gmail.com>
2024-05-18 20:29:09 +0800

    Refactor event handlers in TodoList component
    - Refactored the event handlers in the TodoList component to improve readability and clean up the code.
    - Helper functions were created for 'setNewTodoInput', 'addOnEnter', 'removeTodoList', 'toggleDone', and 'removeTodo'. These separated logic is easier to understand and enhance.
    - Also, unnecessary callback brackets and readOnly attribute have been removed.

--- src/components/organisms/todo/TodoList.tsx
+++ src/components/organisms/todo/TodoList.tsx
@@ -15,28 +15,34 @@ export const TodoList = ({ id, title, handleRemoveTodoList } : {id: number, titl
     handleClearDone
   } = useTodoItem(id);
 
+  const setNewTodoInput = (e: { target: { value: any; }; }) => handleSetNewTodoInput({
+    title: e.target.value,
+    todolist: id
+  })
+
+  const addOnEnter = (event: { key: string; }) => {
+    if (event.key === 'Enter') handleAddTodo();
+  }
+
+  const removeTodoList = () => handleRemoveTodoList(id)
+
   return (
     <div>
       <div className="bg-white rounded shadow p-6 m-4">
         <div className="mb-4">
           <header className="text-grey-darkest flex justify-between">
             <h2>{title} <span>({completedTodo.length}/{todoItems.length})</span></h2>
-            <IoTrash size="20px" className="cursor-pointer" color="gray" onClick={() => handleRemoveTodoList(id)}/>
+            <IoTrash size="20px" className="cursor-pointer" color="gray" onClick={removeTodoList}/>
           </header>
           <div className="flex mt-4">
             <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
                    placeholder="Add Todo"
-                   onChange={(e) => handleSetNewTodoInput({
-                     title: e.target.value,
-                     todolist: id
-                   })}
+                   onChange={setNewTodoInput}
                    value={getNewTodoInput()}
-                   onKeyUp={(event) => {
-                     if (event.key === 'Enter') handleAddTodo();
-                   }}
+                   onKeyUp={addOnEnter}
             />
             <button
-              onClick={() => handleAddTodo()}
+              onClick={handleAddTodo}
               className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-black hover:bg-teal flex items-center">
               <IoAdd size="20px" color="gray"/>
               <span>Add </span>
@@ -62,7 +68,7 @@ export const TodoList = ({ id, title, handleRemoveTodoList } : {id: number, titl
         </fieldset>
         <div className='mt-3'>
           <button
-            onClick={() => handleClearDone()}
+            onClick={handleClearDone}
             className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-black hover:bg-teal flex gap-1 items-center">
             <IoTrash size="20px" color="gray"/>
             <span>Clear finished</span>
@@ -72,28 +78,27 @@ export const TodoList = ({ id, title, handleRemoveTodoList } : {id: number, titl
     </div>
   )
 }
+
 const TodoItem = ({todoItem, toggleTodoDone, handleRemoveTodo}: TodoItemProps) => {
   const doneStyle = todoItem.completed ? 'text-gray-400 line-through' : 'text-gray-900'
+
+  const toggleDone = () => toggleTodoDone(todoItem.id)
+  const removeTodo = () => handleRemoveTodo(todoItem)
 
   return (
     <div className={"flex justify-between items-center border-b-2"}>
       <div className={`flex items-center py-2 flex-grow  ${doneStyle}`}
-           onClick={() => toggleTodoDone(todoItem.id)}>
+           onClick={toggleDone}>
         <input
           checked={todoItem.completed}
           id={"todoItem-" + todoItem.id}
           type="checkbox"
-          onChange={() => {
-          }}
           className="w-4 h-4 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
-          readOnly
         />
         <label htmlFor={"todoItem-" + todoItem.id}
                className="ms-2 text-md font-medium pointer-events-none">{todoItem.title}</label>
       </div>
-      <IoClose size="20px" color="gray" className='cursor-pointer' onClick={e => {
-        handleRemoveTodo(todoItem)
-      }}/>
+      <IoClose size="20px" color="gray" className='cursor-pointer' onClick={removeTodo}/>
     </div>
   );
 }
